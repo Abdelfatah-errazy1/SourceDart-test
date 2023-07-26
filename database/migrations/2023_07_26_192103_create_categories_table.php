9af04d4ddc0ca3b6551d814237c9dc768cf8cd85
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-26 20:44:12 +0100

    creating migration of database

--- database/migrations/2023_07_26_192103_create_categories_table.php
+++ database/migrations/2023_07_26_192103_create_categories_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+      
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',150);            
+            $table->foreignId('parent_id')->references('id')->on('categories')->onDelete('cascade')->nullable();
             $table->timestamps();
         });
+        
     }
 
     /**
