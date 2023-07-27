49068494866311e37fb479f27dbcfa161d683ee7
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-27 11:46:37 +0100

    updatin web.php for product and categories

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+// routes/web.php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('products.')->controller(ProductController::class)->group(function(){
+    Route::get('/products',  'index')->name('index');
+    Route::get('/products/create',  'create')->name('create');
+    Route::post('/products',  'store')->name('store');
+    Route::get('/products/{product}',  'edit')->name('edit');
+    Route::put('/products/{product}',  'update')->name('update');
 });
+
+Route::name('categories.')->controller(CategoryController::class)->group(function(){
+    Route::get('/categories',  'index')->name('index');
+    Route::get('/categories/create',  'create')->name('create');
+    Route::post('/categories',  'store')->name('store');
+    Route::get('/categories/{product}',  'edit')->name('edit');
+    Route::put('/categories/{product}',  'update')->name('update');
+});
+
