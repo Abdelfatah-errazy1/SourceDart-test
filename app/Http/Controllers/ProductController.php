b65f73c7addec7f83fb10058449780a6740c81c1
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-28 21:11:05 +0100

    clean the controller of the product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-// app/Http/Controllers/ProductController.php
 
 use Illuminate\Http\Request;
 use App\Interfaces\ProductServiceInterface;
@@ -18,8 +17,9 @@ class ProductController extends Controller
 
     public function index(Request $request)
     {
-        // Get the sorting and filtering parameters from the request
-        $sortBy = $request->input('sort', 'name'); // Default sorting by name
+       
+        // Get the sorting and filtering 
+        $sortBy = $request->input('sort', 'name'); 
         $category = $request->input('category');
 
         // Fetch paginated products with sorting and filtering
@@ -27,73 +27,66 @@ class ProductController extends Controller
         $categories = $this->productService->getAllCategories();
         $products->appends(['category' => $category]);
         $products->appends(['sort' => $sortBy]);
-        // dd($products);
+
         // Return the view with the paginated product list
         return view('pages.products.index', compact('products','categories'));
     }
 
     public function create()
     {
-        // Fetch all categories to populate the category dropdown in the create form
         $categories = $this->productService->getAllCategories();
-
-        // Return the view with the form to create a product
         return view('pages.products.create', compact('categories'));
     }
 
     public function edit($productId)
     {
-        // Get the product details from the service based on the product ID
         $product = $this->productService->getProductById($productId);
-
-        // Fetch all categories to populate the category dropdown in the edit form
         $categories = $this->productService->getAllCategories();
-
-        // Pass the product and categories data to the view
         return view('pages.products.edit', compact('product', 'categories'));
     }
 
     public function store(Request $request)
     {
-        // Validate the incoming product data
+        // Validate  data
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'required|string',
             'price' => 'required|numeric|min:0',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'categories' => 'array', // Assuming categories are passed as an array of category IDs
+            'categories' => 'required|array',
+            'categories.*' => 'exists:categories,id',
         ]);
 
-        // Handle image upload and get the image path
+        // Handle image path
         $imagePath = $this->handleImageUpload($request->file('image'));
 
         // Create the product and attach categories
         $product = $this->productService->createProduct($validatedData, $imagePath);
 
-        // Redirect to the product listing page with a success message
+        // Redirect to the product listing 
         return redirect()->route('products.index')->with('success', 'Product created successfully.');
     }
 
     public function destroy($productId)
     {
-        // Delete the product using the service
+        // Delete the product 
         $this->productService->deleteProduct($productId);
 
-        // Redirect to the product listing page with a success message
+        // Redirect to the product listing 
         return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
     }
     public function update(Request $request, $productId)
     {
-        // Validate the incoming product data
+        // Validate  data
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'required|string',
             'price' => 'required|numeric|min:0',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'categories' => 'array', // Assuming categories are passed as an array of category IDs
+            'categories' => 'array', 
         ]);
 
-        // Check if a new image was uploaded, and handle image upload if applicable
+        // Check if a new image was uploaded,
         if ($request->hasFile('image')) {
             $validatedData['image'] = $this->handleImageUpload($request->file('image'));
         }
@@ -101,7 +94,7 @@ class ProductController extends Controller
         // Update the product using the service
         $this->productService->updateProduct($productId, $validatedData);
 
-        // Redirect to the product listing page with a success message
+        // Redirect to the product listing 
         return redirect()->route('products.index')->with('success', 'Product updated successfully.');
     }
 
