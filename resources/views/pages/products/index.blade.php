2108855b1fae5f21e599a08dbd71e05dc0aaedc1
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-29 13:08:33 +0100

    clean the index view

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
-@section('content')
+@section('title', 'List of Products')
 
+@section('content')
 
 <section style="background-color: #eee;">
   <div class="container py-5">
@@ -10,7 +11,7 @@
           <li class="breadcrumb-item"><a href="https://www.sourcedart.org/">Home</a></li>
           <li class="breadcrumb-item active" aria-current="page">Products</li>
         </ol>
-      </nav>
+    </nav>
     <form action="{{ route('products.index') }}" method="GET" class="form-inline mb-3">
       <div class="form-group">
           <label for="category">Filter by Category:</label>
@@ -42,7 +43,20 @@
                 <div class="card-body">
                     <div class="text-center">
                     <h5 class="card-title">{{ $product->name }}</h5>
-                    <p class="text-muted mb-4">{{ $product->description }}</p>
+                    <div class="description">
+                        @if (strlen($product->description) > 60)
+                            <span class="truncated">
+                                {{ substr($product->description, 0, 60) }}...
+                            </span>
+                            <span class="full" style="display: none;">
+                                {{ $product->description }}
+                            </span>
+                            <a href="#" class="read-more">Read more</a>
+                        @else
+                            {{ $product->description }}
+                        @endif
+                    </div>
+                    
                     </div>
 
                     <div class="d-flex justify-content-between total font-weight-bold mt-4">
@@ -55,64 +69,31 @@
     </div>
     <div class="d-flex justify-content-center my-4">
         <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
+            <ul class="pagination">
                 <!-- Previous Page Link -->
                 @if ($products->onFirstPage())
                     <li class="page-item disabled">
-                        <span class="page-link" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </span>
+                        <span class="page-link">&laquo;</span>
                     </li>
                 @else
                     <li class="page-item">
-                        <a class="page-link" href="{{ $products->previousPageUrl() }}" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
+                        <a class="page-link" href="{{ $products->previousPageUrl() }}" rel="prev">&laquo;</a>
                     </li>
                 @endif
-
-                 <!-- Page Links (display only two pages) -->
-                 @if ($products->lastPage() > 1)
-                 @if ($products->currentPage() == 1)
-                     <li class="page-item {{ $products->currentPage() == 1 ? 'active' : '' }}">
-                         <a class="page-link" href="{{ $products->url(1) }}">1</a>
-                     </li>
-                     @if ($products->currentPage() + 1 <= $products->lastPage())
-                         <li class="page-item">
-                             <a class="page-link" href="{{ $products->url(2) }}">2</a>
-                         </li>
-                     @endif
-                 @elseif ($products->currentPage() == $products->lastPage())
-                     @if ($products->currentPage() - 1 >= 1)
-                         <li class="page-item">
-                             <a class="page-link" href="{{ $products->url($products->currentPage() - 1) }}">{{ $products->currentPage() - 1 }}</a>
-                         </li>
-                     @endif
-                     <li class="page-item {{ $products->currentPage() == $products->lastPage() ? 'active' : '' }}">
-                         <a class="page-link" href="{{ $products->url($products->currentPage()) }}">{{ $products->currentPage() }}</a>
-                     </li>
-                 @else
-                     <li class="page-item {{ $products->currentPage() == $products->lastPage() - 1 ? 'active' : '' }}">
-                         <a class="page-link" href="{{ $products->url($products->currentPage()) }}">{{ $products->currentPage() }}</a>
-                     </li>
-                     <li class="page-item {{ $products->currentPage() == $products->lastPage() ? 'active' : '' }}">
-                         <a class="page-link" href="{{ $products->url($products->currentPage() + 1) }}">{{ $products->currentPage() + 1 }}</a>
-                     </li>
-                 @endif
-             @endif
-
+        
+                <!-- Current Page  Link -->
+                <li class="page-item active">
+                    <span class="page-link">{{ $products->currentPage() }}</span>
+                </li>
+        
                 <!-- Next Page Link -->
                 @if ($products->hasMorePages())
                     <li class="page-item">
-                        <a class="page-link" href="{{ $products->nextPageUrl() }}" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
+                        <a class="page-link" href="{{ $products->nextPageUrl() }}" rel="next">&raquo;</a>
                     </li>
                 @else
                     <li class="page-item disabled">
-                        <span class="page-link" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </span>
+                        <span class="page-link">&raquo;</span>
                     </li>
                 @endif
             </ul>
@@ -121,87 +102,3 @@
   </div>
 </section>
 @endsection
-
-<!-- resources/views/products/index.blade.php -->
-
-{{-- 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class="mb-4">Product Listing</h1>
-            
-            <!-- Filter and Sort Form -->
-            <form action="{{ route('products.index') }}" method="GET" class="form-inline mb-3">
-                <div class="form-group">
-                    <label for="category">Filter by Category:</label>
-                    <select name="category" id="category" class="form-control mr-2">
-                        <option value="">All Categories</option>
-                        @foreach($categories as $category)
-                            <option value="{{ $category->id }}" {{ request('category') == $category->id ? 'selected' : '' }}>
-                                {{ $category->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="sort">Sort By:</label>
-                    <select name="sort" id="sort" class="form-control ml-2">
-                        <option value="name" {{ request('sort') == 'name' ? 'selected' : '' }}>Name</option>
-                        <option value="price" {{ request('sort') == 'price' ? 'selected' : '' }}>Price</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary ml-2">Apply</button>
-            </form>
-
-            <!-- Product Listing Table -->
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Image</th>
-                        <th>Categories</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($products as $product)
-                        <tr>
-                            <td>{{ $product->name }}</td>
-                            <td>{{ $product->description }}</td>
-                            <td>{{ $product->price }}</td>
-                            <td><img src="{{ asset($product->image) }}" alt="{{ $product->name }}" width="100"></td>
-                            <td>
-                                @foreach($product->categories as $category)
-                                    <span class="badge badge-info">{{ $category->name }}</span>
-                                @endforeach
-                            </td>
-                            <td>
-                                <!-- View Product button -->
-                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-info btn-sm">View</a>
-                                <!-- Edit Product button -->
-                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                                <!-- Delete Product button with a form to perform a DELETE request -->
-                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display: inline-block;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="6" class="text-center">No products found.</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-
-            <!-- Pagination links -->
-            {{ $products->links() }}
-        </div>
-    </div>
-</div>
-@endsection --}}
