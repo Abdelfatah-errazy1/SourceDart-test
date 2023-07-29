674c58b136a620f594df31a2e694eee1286b1d35
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-29 21:21:29 +0100

    fixing the problem of adding parrent category

--- app/Console/Commands/CreateCategory.php
+++ app/Console/Commands/CreateCategory.php
@@ -7,17 +7,42 @@ use App\Models\Category;
 
 class CreateCategory extends Command
 {
-    // Get the category name from the command argument
-    protected $signature = 'category:create {name}';
+    protected $signature = 'category:create 
+                            {name : The name of the category}
+                            {--parent= : The name of the parent category (optional)}';
     protected $description = 'Create a new category';
 
     public function handle()
     {
         $name = $this->argument('name');
-        // Create a new category with the provided name
-        Category::create(['name' => $name]);
+        $parent_id = $this->option('parent');
+
+        // Check if the parent category 
+        if ($parent_id) {
+            $parentCategory = Category::findOrFail($parent_id);
+
+            // Check if the parent category exists
+            if (!$parentCategory) {
+                $this->error("Parent category '$parent_id' does not exist.");
+                return;
+            }
+
+            // Create a new category with name and parent
+            $category = Category::create([
+                'name' => $name,
+                'parent_id' => $parentCategory->id,
+            ]);
+        } else {
+            // Create a new category with name only 
+            $category = Category::create(['name' => $name]);
+        }
+
         // Output success message
         $this->info("Category '$name' created successfully.");
+
+        // If the category has a parent
+        if ($parent_id) {
+            $this->info("Parent Category: $parent_id");
+        }
     }
 }
-
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable=['name'];
+    protected $fillable=['name','parent_id'];
 
     
     //relationship to the parent category
