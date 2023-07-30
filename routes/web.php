60b26fe15e2eec2ddf94f18edb0da3c022d5a8e7
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-07-30 14:43:07 +0100

    updating web php

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,13 @@ use Illuminate\Support\Facades\Route;
 // routes/web.php
 
 use App\Http\Controllers\ProductController;
-
-Route::name('products.')->controller(ProductController::class)->group(function(){
-    Route::get('/products',  'index')->name('index');
-    Route::get('/products/create',  'create')->name('create');
-    Route::post('/products',  'store')->name('store');
-    Route::get('/products/{product}',  'edit')->name('edit');
-    Route::put('/products/{product}',  'update')->name('update');
+Route::redirect('/','products');
+Route::name('products.')->prefix('products')->controller(ProductController::class)->group(function(){
+    Route::get('',  'index')->name('index');
+    Route::get('/create',  'create')->name('create');
+    Route::post('/store',  'store')->name('store');
+    Route::get('/edit/{product}',  'edit')->name('edit');
+    Route::put('/update/{product}',  'update')->name('update');
 });
 
-Route::name('categories.')->controller(CategoryController::class)->group(function(){
-    Route::get('/categories',  'index')->name('index');
-    Route::get('/categories/create',  'create')->name('create');
-    Route::post('/categories',  'store')->name('store');
-    Route::get('/categories/{product}',  'edit')->name('edit');
-    Route::put('/categories/{product}',  'update')->name('update');
-});
 
